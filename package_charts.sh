02f65aca494d7a33d4542f83aa1ca1265e007bd2
Ivan Kazimirau <ikazimirau@onairent.live>
2025-09-17 12:39:42 +0200

    Helm repo charts updated

---
+++ package_charts.sh
@@ -0,0 +1,4 @@
+helm package raven-bundle/
+helm package raven-setup/
+helm package raven-compact/
+helm repo index . --url https://dragonisle.github.io/raven-helm
---
+++ pre_install_compact.sh
@@ -0,0 +1,3 @@
+helm install raven-setup-postgresql bitnami/postgresql
+helm install raven-setup-redis bitnami/redis
+helm install raven-setup-clickhouse bitnami/clickhouse
